f0a58a1bcc030860f81a57116cd710d5408ca856
achelwacalvin-sketch <achelwacalvin@gmail.com>
2026-07-27 17:22:56 +0300

    Implement user authentication functions in script.js

--- script.js
+++ script.js
@@ -1,18 +1,81 @@
+function signUp(){
+
+let email=document.getElementById("email").value;
+let password=document.getElementById("password").value;
+
+
+createUserWithEmailAndPassword(auth,email,password)
+
+.then(()=>{
+
+document.getElementById("message").innerHTML=
+"Account created successfully";
+
+})
+
+.catch(error=>{
+
+document.getElementById("message").innerHTML=
+error.message;
+
+});
+
+}
+
+
+
 function login(){
 
-let name=document.getElementById("username").value;
+let email=document.getElementById("email").value;
+let password=document.getElementById("password").value;
 
-if(name){
+
+signInWithEmailAndPassword(auth,email,password)
+
+.then(()=>{
 
 document.querySelector(".login").style.display="none";
 
 document.getElementById("home").style.display="block";
 
-document.getElementById("userName").innerHTML=name;
+})
+
+.catch(error=>{
+
+document.getElementById("message").innerHTML=
+error.message;
+
+});
 
 }
 
+
+
+function logout(){
+
+signOut(auth);
+
+location.reload();
+
 }
+
+
+
+onAuthStateChanged(auth,(user)=>{
+
+if(user){
+
+document.querySelector(".login").style.display="none";
+
+document.getElementById("home").style.display="block";
+
+document.getElementById("userName").innerHTML=
+user.email;
+
+}
+
+});
+
 
 
 function createPost(){
@@ -26,8 +89,7 @@ let post=document.createElement("div");
 post.className="post";
 
 post.innerHTML=
-"<b>"+document.getElementById("userName").innerHTML+
-"</b><p>"+text+"</p>";
+"<b>"+auth.currentUser.email+"</b><p>"+text+"</p>";
 
 document.getElementById("posts").prepend(post);
 
